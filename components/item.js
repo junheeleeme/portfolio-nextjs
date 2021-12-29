7aa6c2ccbe9cd50bf4b0cb37619c0c67f0887d2f
ju_ni <38034518+junheeleeme@users.noreply.github.com>
2021-12-30 03:30:38 +0900

    feat: contact page 완료

---
+++ components/item.js
@@ -0,0 +1,13 @@
+import Gallery from "react-photo-gallery"
+
+
+const Item = () => {
+
+    return(
+        <>
+            
+        </>
+    )
+}
+
+export default Item
--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,22 @@
 import '../styles/globals.css'
 import Wrapper from '../components/wrapper'
 import theme from '../theme/theme'
+import { extendTheme } from '@chakra-ui/react'
 import { ChakraProvider } from '@chakra-ui/react'
 import { createBreakpoints } from '@chakra-ui/theme-tools'
 
+const breakpoints = createBreakpoints({
+  sm: '480px',
+  md: '768px',
+  lg: '960px',
+  xl: '1280px',
+  'menu' : '640px'
+})
 
 
 const MyApp = ({ Component, pageProps }) => {
+
+  const theme = extendTheme({ breakpoints });
 
   return (
     <>
