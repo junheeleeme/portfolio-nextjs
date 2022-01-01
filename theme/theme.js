ab9a76f2ddd0f6f85128bf1dbdae80645e06cab6
ju_ni <38034518+junheeleeme@users.noreply.github.com>
2022-01-02 02:01:44 +0900

    feat: portfolio 상세 페이지 작업중

--- theme/theme.js
+++ theme/theme.js
@@ -1,12 +1,18 @@
-import { extendTheme } from '@chakra-ui/react'
+import { extendTheme } from "@chakra-ui/react"
+import { breakpoints } from "./breakpoints";
 
-// 2. Add your color mode config
-const config = {
-    initialColorMode: 'light', 
-    useSystemColorMode: false, //initialColorMode를 알 수 없는 경우 true일 때, system 기본값으로 사용
-}
+export const theme = extendTheme({
+    config :{
+        initialColorMode : 'light',
+        cssVarPrefix: 'macjjuni'
+    },
+    colors: {
+        dark_toggleBg : '#845EC2',
+        light_toggleBg : '#F9F871',
+    },
+    breakpoints,
+});
 
-// 3. extend the theme
-const theme = extendTheme({ config })
 
-export default theme
+
+
