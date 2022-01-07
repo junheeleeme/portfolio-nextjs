ff3a2ff19b6b1b57143dd3738c0e49b01c5348ce
ju_ni <38034518+junheeleeme@users.noreply.github.com>
2022-01-08 00:46:17 +0900

    feat: fixed duplicate props

--- components/voxel.js
+++ components/voxel.js
@@ -1,10 +1,8 @@
 import { useRef, useEffect, useState } from "react"
 import { Box, Center, Spinner } from "@chakra-ui/react";
-import { useColorMode } from "@chakra-ui/react";
 
 const Voxel = () => {
     
-    const { colorMode } = useColorMode();
     const [loaded, setLoaded] = useState(false);
     const view = useRef(null); //속성 변화 감지 노드
     
@@ -12,11 +10,8 @@ const Voxel = () => {
     useEffect(()=>{
         const interval = setInterval(() => {
             if(view.current.loaded === true) {
-                console.log("로딩 완료");
                 setLoaded(true);
                 clearInterval(interval);
-            }else{
-                console.log("로딩 체크");
             }
         }, 500);
     }, []);
@@ -28,13 +23,13 @@ const Voxel = () => {
                 {
                     loaded === false ?
                     <Center position='absolute' top='0' left='0' w='100%' h='100%' zIndex='9999'>
-                        <Spinner thickness='4px' speed='0.65s' emptyColor='gray.200' color='gray.800' emptyColor='gray.300' size='xl' />
+                        <Spinner thickness='4px' speed='0.65s' color='gray.800' emptyColor='gray.300' size='xl' />
                     </Center>
                     :
                     <></>
                 }
                 <Box position='relative' opacity={loaded === true ? '1' : '0'} transition='opacity 1s ease'>
-                    <model-viewer ref={view} src='/voxel_jjuni-official.gltf' camera-controls auto-rotate  disable-zoom='true'
+                    <model-viewer ref={view} src='/voxel_jjuni-official.gltf' camera-controls auto-rotate  disable-zoom='false'
                     shadow-intensity="3" autoplay environment-image="neutral" alt='꾸생 아바타' 
                     style={{width : '100%', height : '450px' ,'--progress-bar-height' : '0' , '--progress-mask' : 'none'}}/>
                 </Box>
