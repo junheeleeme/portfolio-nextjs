1031cb77eaf8f2d941602207cd098e9383f0e126
ju_ni <38034518+junheeleeme@users.noreply.github.com>
2022-01-11 07:14:37 +0900

    fixed deploy

---
+++ components/content_list.js
@@ -0,0 +1,64 @@
+import Link from "next/link"
+import { SimpleGrid, Box, Link as LinkStyled, Text, Button, Image, Heading, Spinner } from '@chakra-ui/react'
+
+const Content_List = ({posts}) => {
+    console.log(posts);
+    return(
+        <Box>
+            {
+                posts === undefined ?
+                <Spinner/>
+                    :
+                <>    
+                <Heading as='h2' size='md' pb={{base: '30px', sm: '10px'}}>최근 포스트</Heading>
+
+                <SimpleGrid columns={{base: 1, sm: 2, md: 3}} spacingX='20px' spacingY={{base: '30px', sm : '15px'}}
+                p={{base : '0 15px', sm : '20px 0'}}>
+                
+                {
+                    posts.map((p, idx)=>
+                    
+                        <Box key={p.title+idx} position='relative' w='100%' overflow='hidden'>
+                            <Link href={`https://juni-official.tistory.com${p.link}`} passHref>
+                                <LinkStyled _hover={false}  target='_blank'>
+
+                            <Image src={p.thumb} alt={p.title} w='100%' borderRadius='4px'/>
+
+                            <Heading as='h3' size='sm'  position='relative' w='100%' p='5px 2px'
+                            textOverflow='ellipsis' whiteSpace='nowrap' overflow='hidden'>{p.title}</Heading>
+
+                            <Box position='relative' h='22px' fontSize='0.8em'>
+                                <Text position='absolute' top='0' left='4px'>{p.cate}</Text>
+                                <Text position='absolute' top='0' right='4px'>{p.date}</Text>
+                            </Box>
+
+                                </LinkStyled>
+                            </Link>
+                        </Box>
+                    )
+                }
+
+                <Box display={{base:'none', sm: 'block', md:'none'}} position='relative' w='100%'>
+                    <Link href='https://juni-official.tistory.com' passHref>
+                        <LinkStyled target='_blank' display='inline-block' w='100%' height='100%' fontSize="1.1em">
+                            <Text position='absolute' top='50%' left='50%' transform='translate(-50%, -50%)'>더보기</Text>
+                        </LinkStyled>
+                    </Link>
+                </Box>
+
+                </SimpleGrid>
+
+                <Box p={{base: '15px', sm: '0', md: '0 0 20px 0'}}>
+                    <Link href='https://juni-official.tistory.com' passHref>
+                        <LinkStyled _hover={false} target='_blank'>
+                            <Button display={{base: 'block', sm: 'none', md:'block'}} w='100%' variant='outline'>더보기</Button>
+                        </LinkStyled>
+                    </Link>
+                </Box>
+                </>
+            }
+        </Box>
+    )
+}
+
+export default Content_List
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
-import { Wrap, WrapItem, Center, Image, Box, UnorderedList, ListItem, Heading,
-SimpleGrid, Link as LinkStyled, Text, Button } from "@chakra-ui/react"
-import Link from "next/link"
 import axios from "axios"
+import { Wrap, WrapItem, Center, Image, Box, UnorderedList, ListItem, Heading } from "@chakra-ui/react"
+import Content_List from "../components/content_list"
 
 const index = ({posts})=> {
 
@@ -42,63 +41,18 @@ const index = ({posts})=> {
 
       </Wrap>
 
-
-      <Box>
-
-        <Heading as='h2' size='md' pb={{base: '30px', sm: '10px'}}>최근 포스트</Heading>
-
-        <SimpleGrid columns={{base: 1, sm: 2, md: 3}} spacingX='20px' spacingY={{base: '30px', sm : '15px'}}
-        p={{base : '0 15px', sm : '20px 0'}}>
-          
-          {
-            posts.map((p, idx)=>
-            
-                <Box key={p.title+idx} position='relative' w='100%' overflow='hidden'>
-                  <Link href={`https://juni-official.tistory.com${p.link}`} passHref>
-                    <LinkStyled _hover={false}  target='_blank'>
-                  <Image src={p.thumb} alt={p.title} w='100%' borderRadius='4px'/>
-                  <Heading as='h3' size='sm'  position='relative' w='100%' p='5px 2px'
-                  textOverflow='ellipsis' whiteSpace='nowrap' overflow='hidden'>{p.title}</Heading>
-                  <Box position='relative' h='22px' fontSize='0.8em'>
-                    <Text position='absolute' top='0' left='4px'>{p.cate}</Text>
-                    <Text position='absolute' top='0' right='4px'>{p.date}</Text>
-                  </Box>
-                    </LinkStyled>
-                  </Link>
-                </Box>
-            )
-          }
-
-          <Box display={{base:'none', sm: 'block', md:'none'}} position='relative' w='100%'>
-            <Link href='https://juni-official.tistory.com' passHref>
-              <LinkStyled target='_blank' display='inline-block' w='100%' height='100%' fontSize="1.1em">
-                <Text position='absolute' top='50%' left='50%' transform='translate(-50%, -50%)'>더보기</Text>
-              </LinkStyled>
-            </Link>
-          </Box>
-
-        </SimpleGrid>
-
-        <Box p={{base: '15px', sm: '0', md: '0 0 20px 0'}}>
-          <Link href='https://juni-official.tistory.com' passHref>
-            <LinkStyled _hover={false} target='_blank'>
-              <Button display={{base: 'block', sm: 'none', md:'block'}} w='100%' variant='outline'>더보기</Button>
-            </LinkStyled>
-          </Link>
-        </Box>
-        
-      </Box>
+      <Content_List posts={posts}/>
+      
     </Box>
   )
 }
 
-
 export const getServerSideProps = async() => {
 
-  const { data } = await axios('http://localhost:3000/api/posts');
+  const { data } = await axios('http://localhost/api/posts');
 
   return{
-    props : { posts : data }
+      props : { posts : data }
   }
 }
 
