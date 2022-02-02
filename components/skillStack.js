8bde16c39fc8c5f86e4f35eccde402862c09e02c
ju_ni <38034518+junheeleeme@users.noreply.github.com>
2022-02-02 14:00:26 +0900

    feat: import fixed

--- components/skillStack.js
+++ components/skillStack.js
@@ -1,7 +1,7 @@
 import { Box, Tooltip, Center, Heading, HStack } from "@chakra-ui/react"
 import { SiReact, SiJavascript, SiNodedotjs, SiHtml5, SiCss3, SiJquery, SiStyledcomponents,
     SiNextdotjs, SiMongodb, SiRedux, SiAmazonaws, SiMysql, SiSvelte, SiVuedotjs, SiFlutter } from 'react-icons/si'
-import { BsFillEmojiSmileFill, BsFillEmojiLaughingFill, BsFillEmojiHeartEyesFill } from 'react-icons/Bs'
+import { BsFillEmojiSmileFill, BsFillEmojiLaughingFill, BsFillEmojiHeartEyesFill } from 'react-icons/bs'
 import { useColorModeValue } from "@chakra-ui/react"
 
 const SkillStack = () => {
